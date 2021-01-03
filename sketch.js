f8c00042ef9de51286b23b17e007d26c698edc44
kirtivasan <73664592+kirtivasan@users.noreply.github.com>
2021-01-03 21:20:41 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,105 @@
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-var dustbinObj, paperObject,groundObject;	
-var world;
-var binIMG;
-var paperIMG;
+var monkey , monkey_running
+var banana ,bananaImage, bGroup, obstacle, obstacleImage, oGroup;
+var FoodGroup, obstacleGroup;
+var Banana_score;
+var ground;
+var PLAY=1,END=0,gamestate=1;
 
 function preload(){
-
-	binIMG=loadImage("dustbingreen.png");
-	paperIMG=loadImage("paper.png");
-}
-function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
-
-	
-
-
-	engine = Engine.create();
-	world = engine.world;
-	dustbinObj=new dustbin(1200,650);
-	paperObject=new paper(200,450,40);
-	groundObject=new ground(width/2,670,width,20);
-	//Create a Ground
-	
-
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
-
-	Engine.run(engine);
-	//Render.run(render);
   
+  
+  monkey_running = loadAnimation("sprite_0.png",
+                                 "sprite_1.png",
+                                 "sprite_2.png",
+                                 "sprite_3.png",
+      "sprite_4.png",
+    "sprite_5.png",
+   "sprite_6.png",
+ "sprite_7.png",
+"sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+ createCanvas(460,460);
+  monkey=createSprite(70,400,20,20);
+  monkey.addAnimation("running",monkey_running);
+  monkey.scale=0.1;
+  
+  ground=createSprite(230,435,500,10);
+
+   Banana_score=0;
+    
+  bGroup = new Group();
+  oGroup = new Group();
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
- 
-  dustbinObj.display();
-  paperObject.display();
-  groundObject.display();
+ background("white");
   
- 
+  if (gamestate===PLAY){
+  if(keyDown("space")&& monkey.y>399){
+    monkey.velocityY=-23;
+  }
   
+  if(monkey.isTouching(oGroup)){
+     gamestate=END;
+     }
+
+  monkey.velocityY=monkey.velocityY+1;
   
- 
+  Banana();  
+  Obstacles();
+}else{
+    Death();
+}
+  drawSprites();
+  monkey.collide(ground);
+  monkey.collide(ground);
+  BananaScore();
 }
 
-function keyPressed() {
-  	if (keyCode === UP_ARROW) {
-
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-    
-  	}
+function Banana(){
+  if(frameCount%100===0){
+      banana=createSprite(450,Math.round(random(170,250)),20,20);
+      banana.velocityX=-3;
+      banana.addImage(bananaImage);
+      banana.lifetime=150;
+      banana.scale=0.1;
+      bGroup.add(banana);
+     }
 }
-
-
-
-
+function Obstacles(){
+   if(frameCount%200===0){
+      obstacles=createSprite(Math.round(random(400,470)),410,40,40);
+      obstacles.velocityX=-3;
+      obstacles.addImage(obstacleImage);
+      obstacles.lifetime=150;
+      obstacles.scale=0.15;
+      oGroup.add(obstacles);
+      }
+}
+function BananaScore(){
+  text("Score: "+ Banana_score, 230,50);
+  if(monkey.isTouching(bGroup)){
+      Banana_score=Banana_score+1;
+      bGroup.destroyEach();
+     }
+}
+function Death(){
+    oGroup.setVelocityXEach(0);
+    bGroup.setVelocityXEach(0);
+    oGroup.setLifetimeEach(0);
+    bGroup.setLifetimeEach(0);
+    Banana_score=0;
+  
+}
 
